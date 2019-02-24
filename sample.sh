637103cc71c4184a656d6005a3e8c4e8fb4f1f42
Tim Speetjens <tim.speetjens@gmail.com>
2019-02-24 12:45:45 +0100

    Increase timeout, reflect presentation values

--- sample.sh
+++ sample.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+oc login -u developer
+
+oc new-project myapp --display-name="My Application"
 
 oc new-app --strategy=source --image-stream=jboss-webserver31-tomcat8-openshift:1.2 https://github.com/timspeetjens/os-sample-java-web.git --name sample
 oc expose svc/sample
